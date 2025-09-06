f3dd74b7258efc7fbb63bbeae15248ccf011b9e3
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-09-06 09:56:27 +0530

    created the user and projecr entity

---
+++ src/main/java/com/company/collabSphere_backend/entity/Project.java
@@ -0,0 +1,54 @@
+package com.company.collabSphere_backend.entity;
+
+
+import com.company.collabSphere_backend.enums.ProjectStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.locationtech.jts.geom.Point;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "projects")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false)
+    private String repoUrl;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User owner;
+
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ProjectStatus status = ProjectStatus.PENDING;
+
+
+    @Column(columnDefinition = "geometry(Point, 4326)")
+    private Point location;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
