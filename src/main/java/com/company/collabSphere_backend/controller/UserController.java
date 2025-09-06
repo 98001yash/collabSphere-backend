50f919eacfb824aed47c8026b1c6165ee191feb4
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-09-06 11:39:28 +0530

    created the user controller and implemented the API

---
+++ src/main/java/com/company/collabSphere_backend/controller/UserController.java
@@ -0,0 +1,56 @@
+package com.company.collabSphere_backend.controller;
+
+
+import com.company.collabSphere_backend.dtos.UserRequestDto;
+import com.company.collabSphere_backend.dtos.UserResponseDto;
+import com.company.collabSphere_backend.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDto> registerUser(@Valid @RequestBody UserRequestDto userRequestDto){
+        log.info("Registering user with email: {}",userRequestDto.getEmail());
+        return ResponseEntity.ok(userService.registerUser(userRequestDto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponseDto>> getAllUsers(){
+        log.info("Fetching all users");
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDto> getUserById(@PathVariable Long id){
+        log.info("Fetching  user with id: {}",id);
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @PatchMapping("/{id}/bio")
+    public ResponseEntity<UserResponseDto> updateUserBio(
+            @PathVariable Long id,
+            @RequestBody String newBio
+    ) {
+        log.info("Updating bio for user ID: {}", id);
+        return ResponseEntity.ok(userService.updateBio(id, newBio));
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        log.info("Deleting user with ID: {}", id);
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+}
