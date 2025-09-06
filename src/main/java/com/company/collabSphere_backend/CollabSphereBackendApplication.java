ceee81eb044a8fbf8b03fc965a7e20de8d9f26b8
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-09-06 09:33:43 +0530

    intialized the project

---
+++ src/main/java/com/company/collabSphere_backend/CollabSphereBackendApplication.java
@@ -0,0 +1,13 @@
+package com.company.collabSphere_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabSphereBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabSphereBackendApplication.class, args);
+	}
+
+}
