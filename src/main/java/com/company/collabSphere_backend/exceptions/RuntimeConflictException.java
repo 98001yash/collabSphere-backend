cba2b5bc6fb0423f10fb25bde4cb23a87102ae73
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-09-06 09:38:20 +0530

    added the exceptions, advices, config

---
+++ src/main/java/com/company/collabSphere_backend/exceptions/RuntimeConflictException.java
@@ -0,0 +1,11 @@
+package com.company.collabSphere_backend.exceptions;
+
+public class RuntimeConflictException extends RuntimeException{
+    public RuntimeConflictException() {
+    }
+
+    public RuntimeConflictException(String message) {
+        super(message);
+    }
+}
+
