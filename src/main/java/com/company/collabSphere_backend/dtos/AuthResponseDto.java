faabc2ea3c892ef3aa21b22acfc79186a89f470c
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-09-07 09:45:35 +0530

    created the loginRequest and authResponseDto

---
+++ src/main/java/com/company/collabSphere_backend/dtos/AuthResponseDto.java
@@ -0,0 +1,15 @@
+package com.company.collabSphere_backend.dtos;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthResponseDto {
+
+    private String token;
+    private UserResponseDto userResponseDto;
+}
