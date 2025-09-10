90b3b067eea035ead7c603afb51848676886b8ea
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-09-10 12:11:01 +0530

    created the required method in the collaborationRequest repository

--- src/main/java/com/company/collabSphere_backend/repository/CollaborationRequestRepository.java
+++ src/main/java/com/company/collabSphere_backend/repository/CollaborationRequestRepository.java
@@ -5,19 +5,23 @@ import com.company.collabSphere_backend.entity.Project;
 import com.company.collabSphere_backend.entity.User;
 import com.company.collabSphere_backend.enums.CollaborationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.Optional;
 
 public interface CollaborationRequestRepository extends JpaRepository<CollaborationRequest,Long> {
 
-    boolean existsByProjectAndStudent(Project project, User student);
-
     Optional<CollaborationRequest> findByStudentAndProject(User student, Project project);
-
     List<CollaborationRequest> findByProject(Project project);
-
     List<CollaborationRequest> findByStudent(User student);
-    List<CollaborationRequest> findByProjectAndStatus(Project project, CollaborationStatus statue);
+
+    // for student dashboard: get requests made by a student
+    List<CollaborationRequest> findByStudentId(Long studentId);
+
+    // for faculty dashboard: get requests for projects owned by faculty
+    List<CollaborationRequest> findByProjectOwnerId(Long facultyId);
+
+    // (optional) filter by status (Pending/Accepted/rejected)
+    List<CollaborationRequest> findByProjectOwnerIdAndStatus(Long facultyId, CollaborationStatus status);
+
 
 }
