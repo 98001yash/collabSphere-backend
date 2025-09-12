766ac460fe80e8dfc4ed173865b570051bf039a5
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-09-13 00:20:51 +0530

    created the createNotification service API

---
+++ src/main/java/com/company/collabSphere_backend/service/NotificationService.java
@@ -0,0 +1,25 @@
+package com.company.collabSphere_backend.service;
+
+
+import com.company.collabSphere_backend.entity.Notification;
+import com.company.collabSphere_backend.repository.NotificationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationService {
+
+    private final NotificationRepository notificationRepository;
+
+    // create the notification ans save it
+    public Notification createNotification(Long userId, String type,String message, Long relatedEntityId){
+        Notification notification = new Notification();
+        notification.setUserId(userId);
+        notification.setType(type);
+        notification.setMessage(message);
+        notification.setRelatedEntityId(relatedEntityId);
+        notification.setStatus("UNREAD");
+        return notificationRepository.save(notification);
+    }
+}
